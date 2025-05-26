5cda7c521c4e9cd70eaa5370f62c73ea76677638
Luca Steeb <contact@luca-steeb.com>
2025-05-26 16:51:50 +0200

    show profile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,6 +15,24 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-white">
       <Chat />
+
+      <div>
+        <div className="text-sm">
+          Your profile name{" "}
+          <span className="text-xs">(only loaded on the client)</span>
+        </div>
+        <input
+          className="border-2 border-gray-300 rounded-md p-2 w-full"
+          value={me?.profile.name ?? ""}
+          onChange={(e) => {
+            if (!me) {
+              return;
+            }
+
+            me.profile.name = e.target.value;
+          }}
+        />
+      </div>
     </main>
   );
 }
